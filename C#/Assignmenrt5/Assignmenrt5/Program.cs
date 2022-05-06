0a2b9050c7a5fb129af0a5488a2320119ec1f104
RishikeshThote <103927193+RishikeshThote@users.noreply.github.com>
2022-05-06 16:20:50 +0530

    Update Program.cs

--- C#/Assignmenrt5/Assignmenrt5/Program.cs
+++ C#/Assignmenrt5/Assignmenrt5/Program.cs
@@ -6,72 +6,78 @@ using System.Threading.Tasks;
 
 namespace Assignment5
 {
-    class Bank
+    internal class BankException :ApplicationException
     {
-        int money = 32229;
-        String name = "MR.RISHIKESH";
-        String account_Number;
-        static String name_Of_Bank = "Bank of Maharashtra";
-        public int updatedBalance;
-
-        public void deposit()
+        public BankException(string msg) : base(msg)
         {
-            Console.WriteLine("\nEnter the amount you want to deposit:");
-            int depMoney = int.Parse(Console.ReadLine());
-            updatedBalance = money + depMoney;
-            if (depMoney == 0)
-            {
-                Console.WriteLine("Minimum deposit of 150 ");
-            }
-            else
-            {
-                Console.WriteLine("Updated balance of  after deposit : " + updatedBalance);
-            }
 
-        }
-        public void withdraw()
+        } 
+
+    }
+    
+        class Bank
         {
-            try
+            int money=32229;
+            String acc_name="MR.RISHIKESH";
+            String acc_num;
+            static String bank_name = "Bank Of Maharashtra";
+            static public  int updatedBalance;
+            public void depositMoney()
             {
-                Console.WriteLine("Your account balance: " + updatedBalance);
-                Console.WriteLine("\nEnter the amount you want to withdraw:");
-                int withdraw = int.Parse(Console.ReadLine());
-                updatedBalance = updatedBalance - withdraw;
-                if (withdraw > updatedBalance)
+               
+                    Console.WriteLine("\nEnter the amount you want to deposit in acount:");
+                    int depMoney = int.Parse(Console.ReadLine());
+                    
+                    if (depMoney == 0)
+                    {
+                        Console.WriteLine("minimum deposit should be 150 ");
+                    }
+                    else
+                    {
+                        updatedBalance = money + depMoney;
+                        Console.WriteLine("updated balance of " + acc_name + " after deposit is " + updatedBalance);
+                    }
+                
+            }
+            public void withdrawMoney()
+            {
+                try
                 {
-                    Console.WriteLine("Insuffiecient balance");
-                }
-                else
+                    Console.WriteLine("Welcome " + acc_name + " your accountbalance is: " + updatedBalance);
+                    Console.WriteLine("\nEnter the amount you want to withdraw from account:");
+                    int withdraw = int.Parse(Console.ReadLine());
+                    updatedBalance = updatedBalance - withdraw;
+                    if (withdraw > updatedBalance)
+                    {
+                        Console.WriteLine("insuffiecient balance in your account");
+                    }
+                    else
+                    {
+                        Console.WriteLine("updated balance of  " + acc_name + " after withdrawal is " + updatedBalance);
+                    }
+
+                }catch (Exception )
                 {
-                    Console.WriteLine("Your account updated balance after withdrawal : " + updatedBalance);
+                    Console.WriteLine("enter balance in number not in words inputmissmatch exception ");
                 }
-
             }
-            catch (Exception )
+            public void Balance()
             {
-                Console.WriteLine("Enter balance in number not in words input missmatch ");
+                Console.WriteLine("welcome to the " + bank_name + "bank ");
+                Console.WriteLine("Enter account number: ");
+                acc_num = Console.ReadLine();
+                Console.WriteLine("Welcome " + acc_name + " your accountbalance  is : " + money);
+
             }
         }
-
-        public void Balance()
-        {
-            Console.WriteLine("Welcome to the " + name_Of_Bank);
-            Console.WriteLine("Enter account number: ");
-            account_Number = Console.ReadLine();
-
-            Console.WriteLine("Hello " + name + " your savings account balance: " + money);
-
-        }
-
-
-        public static void Main(string[] args)
+   internal class Program 
+    { 
+        static void Main(string[] args)
         {
             Bank bank = new Bank();
             bank.Balance();
-            bank.deposit();
-            bank.withdraw();
-
-
+            bank.depositMoney();
+            bank.withdrawMoney();
             Console.ReadLine();
         }
     }
